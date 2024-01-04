731d0585ebe2444b616fa85366c1e0faf75603a0
fazleyazdan <fazleyazdan345@gmail.com>
2024-01-04 11:11:43 +0500

    Tuple

--- Lists/advanced_lists.py
+++ Lists/advanced_lists.py
@@ -3,49 +3,41 @@
 cars = ['audi', 'ferrari', 'toyota', 'honda']
 print(cars)
 
-print()                         #! empty print() statement is for extra line spacing 
+print()                          
 message = f'my first car was not a {cars[1].title()}'      
 print(message)
-print()
 
 # appending in list
 cars.append('kia')
-
-print("after appending: ")
+print(f"\n after appending: ")   #! '\n' statement is for new line spacing
 for car in cars:                #! using for loop to iterate and print value of list one by one
     print(car)
 
 
 # removing specific value from a list 
-print()
-
 cars.remove('ferrari')
-print("after removing specific value:")
+print(f"\n after removing specific value:")
 print(cars)
 
     
 # reversing the list
-print()
-
 cars.reverse()
-print("after reversing the list: ")
+print(f"\n after reversing the list: ")
 print(cars)
 
 
 # printing the length of a list
-print()
-print("length of cars ", len(cars))
+print(f"\n length of cars ", len(cars))
 
 #  sorting the list by alphabets
-print()
 cars.sort()
-print("after sorting the list:")
+print(f"\n after sorting the list:")
 print(cars)
     
 # making copy of a list
-print()
-cars_copy = cars.copy()
-print("after copying the list:")
+# print()
+cars_copy = cars.copy()                 
+print(f"\n after copying the list:")
 print(cars_copy)
 
 
---
+++ Tuple/Simple.py
@@ -0,0 +1,6 @@
+# define a tuple named dimensions in Python. In this specific case, the tuple contains two elements: 200 and 50. 
+# Tuples are similar to lists but are immutable, meaning their elements cannot be modified after the tuple is created.
+
+dimensions = (200,50)
+
+print(f"first dimension is: {dimensions[0]}")
