5c726656a25f3f7cac9e34ab2d622713c59eb374
fazleyazdan <fazleyazdan345@gmail.com>
2024-01-05 16:50:14 +0500

    tuple and list updated

--- Lists/list_comprehension.py
+++ Lists/list_comprehension.py
@@ -1,10 +1,12 @@
-# range(1, 11): This creates an iterable that includes the numbers from 1 to 10 (inclusive).
 
-# value**2: This is the expression used to calculate the square of each value in the range.
 
-# [value**2 for value in range(1, 11)]: This is a list comprehension that iterates over the numbers from 1 to 10, 
+# 1. value**2: This is the expression used to calculate the square of each value in the range.
 
-# calculates the square of each number, and creates a new list (squares) containing these squared values.
+# 2. range(1, 11): This creates an iterable that includes the numbers from 1 to 10 (inclusive).
+
+# 3. [value**2 for value in range(1, 11)]: This is a list comprehension that iterates over the numbers from 1 to 10, 
+
+# 4. calculates the square of each number, and creates a new list (squares) containing these squared values.
 
 squares = [value**2 for value in range(1,11)]
 
