d5ac3ba4a0ad61e8295e23744aaa6120c4c63862
fazleyazdan <fazleyazdan345@gmail.com>
2024-01-12 16:31:53 +0500

    exercise

--- 3. strings/concatenation.py
+++ 3. strings/concatenation.py
@@ -1,5 +1,6 @@
 
-routine = ['6 AM', '11 PM']
+routine = ['6 AM', '11 PM', '8 PM']
 
 print('my wakeup time is'+ " " + routine[0])
 print('my sleeping time is'+ " " + routine[1])
+print('my gym exercise time is'+ " " + routine[2])
