0e45fdaea0307578f9a3bca2f603266e58984732
fazleyazdan <fazleyazdan345@gmail.com>
2024-01-20 22:34:36 +0500

    working on conditions

---
+++ 6. Conditions_&_operators/if_elif_else.py
@@ -0,0 +1,8 @@
+age = 27
+
+if age >=22:
+    print("you are now mature enough to differentiate between right and wrong")
+elif age ==27:
+    print("you have to control your desires in order to get closer to your creator")
+else:
+    print("devil is always there. he does not sleep, make your defenses strong")
