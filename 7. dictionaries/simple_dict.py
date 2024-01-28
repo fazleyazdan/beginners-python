b2d45f0b2df5558aef954666d24407a5428d8221
yazdan777 <fazleyazdan345@gmail.com>
2024-01-28 22:45:49 +0500

    starting dictionaries

---
+++ 7. dictionaries/simple_dict.py
@@ -0,0 +1,7 @@
+# A dictionary consist of key value pair
+# in the below example 'name' is key and 'audi' is value
+
+dictionary = {'name': 'audi', 'model': '2024'}
+
+# printing a dictionary
+print(dictionary)
