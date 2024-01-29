911df0e454d8ce2f6db198abf08a3a2c58c559ad
fazleyazdan <fazleyazdan345@gmail.com>
2024-01-29 15:48:47 +0500

    advance dictionary

--- 7. dictionaries/dictionaries_advance.py
+++ 7. dictionaries/dictionaries_advance.py
@@ -3,19 +3,21 @@
 # Every key is connected to its value by a colon, and individual key-value pairs are separated by commas.
 
 emp = {'name': 'ali', 'age': '27'}
-print(f"emp dictionary: {emp}")
+print(f"\nemp dictionary: {emp}")
 
 # storing value of a key in a variable
 emp_age = emp['age']
-print(f"\nemploye age is: {emp_age}")
+print(f"employe age is: {emp_age}")
 
 # adding new keyvalue to the dictionary
 emp['designation'] = 'driver'
-print(f"\nupdated emp dictionary: {emp}")
+print(f"updated emp dictionary: {emp}")
 
 # modifying values of in a dictionary
 emp['age'] = '28'
-print(f"\nupdated emp age is: {emp['age']}")
-print("\nupdated dictionary:",emp)
+print(f"updated emp age is: {emp['age']}")
+print("designation added to dictionary:",emp)
 
-
+# deleting key-value pair in dictionary
+del (emp['age'])
+print(f"dictionary after deleting employe_age: {emp}")
