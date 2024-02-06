b669df63f98693c112c7a309d803389cfa681532
fazleyazdan <fazleyazdan345@gmail.com>
2024-02-06 20:59:00 +0500

    assignment 1 completed

--- 7. dictionaries/Assignment_1.py
+++ 7. dictionaries/Assignment_1.py
@@ -39,3 +39,23 @@ print()
 for key, value in fav_num.items():
   print(f"{key}'s fav number is: {value}") 
   
+# A Python dictionary can be used to model an actual dictionary. 
+# However, to avoid confusion, let’s call it a glossary.
+# • Think of five programming words you’ve learned about in the previous chapters. 
+# Use these words as the keys in your glossary, and store their meanings as values.
+# • Print each word and its meaning as neatly formatted output. 
+# You might print the word followed by a colon and then its meaning, 
+# or print the word on one line and then print its meaning indented on a second line. 
+# Use the newline character (\n) to insert a blank line between each word-meaning pair in your output.
+
+py_glossary = {'dictionary': 'key value pair',
+               'loop': 'iteration',
+               'list': 'collection of items',
+               'tuple': 'immutable',
+               'variable': 'a container',
+               }
+
+print("Glossary")
+for key, value in py_glossary.items():
+  print(f"\n{key}")
+  print("   ", value) 
