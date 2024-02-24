cf41a6d924eda60c34422b026a397704dd459e31
fazleyazdan <fazleyazdan345@gmail.com>
2024-02-24 21:07:41 +0500

    user inputs & while loops started

---
+++ 9. userinputs & while loops/userinputs_func.py
@@ -0,0 +1,5 @@
+# The input() function pauses your program and waits for the user to enter some text. 
+# Once Python receives the user’s input, it assigns that input to a variable to make it convenient for you to work with.
+
+message = input("Tell me something, i will repeat it to you:")
+print("\ni think you said : ",message)
