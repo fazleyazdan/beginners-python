2f7f92f98a87f3526137d86e61a5b226f6082a1d
fazleyazdan <fazleyazdan345@gmail.com>
2024-02-28 17:19:02 +0500

    chapter 7 task 3 completed

--- 8. userinputs & while loops/5. assignment.py
+++ 8. userinputs & while loops/5. assignment.py
@@ -24,8 +24,8 @@ for finished in finished_sandwiches:
 # Add code near the beginning of your program to print a message saying the deli has run out of pastrami, 
 # and then use a while loop to remove all occurrences of 'pastrami' from sandwich_orders. 
 # Make sure no pastrami sandwiches end up in finished_sandwiches
-print("==================================================================")
 
+print("==================================================================")
 sandwich_orders = ['pastrami','chicken', 'pastrami','egg','pastrami', 'beef']
 print("\noh No! our restaurant has run out of pastrami")
 while 'pastrami' in sandwich_orders:
@@ -40,3 +40,25 @@ while sandwich_orders:
 print(f"\nready to eat sandwiches")
 for finished in finished_sandwiches:
     print(f"\t{finished} sandwich")
+    
+    
+#! Task 3: Dream Vacation 
+# Write a program that polls users about their dream vacation. 
+# Write a prompt similar to If you could visit one place in the world, where would you go? 
+# Include a block of code that prints the results of the poll
+
+print("==================================================================")
+dream_vacation = {}
+flag = True
+
+while flag:
+    name = input("\nEnter your name: ")
+    dream_place = input("\nIf you could visit one place in the world, where would you go? ")
+    dream_vacation[name] = dream_place
+    response = input("\nwanna take another poll? type 'yes/no': ")
+    if response == 'no':
+        flag = False
+
+print("\n*** Results of poll are as follows:\n")
+for key, val in dream_vacation.items():
+    print(f"\t{key.title()} : {val.title()}")
