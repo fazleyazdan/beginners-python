22d318d56a2293a7c42f961c1093714336036dd7
fazleyazdan <fazleyazdan345@gmail.com>
2024-02-28 16:26:17 +0500

    chapter 7 task 2 completed

--- 9. userinputs & while loops/5. assignment.py
+++ 9. userinputs & while loops/5. assignment.py
@@ -5,15 +5,38 @@
 # As each sandwich is made, move it to the list of finished sandwiches. 
 # After all the sandwiches have been made, print a message listing each sandwich that was made.
 
-sandwich_orders = ['chicken sandwitch', 'egg sandwitch', 'beef sandwitch']
+sandwich_orders = ['chicken', 'egg', 'beef']
 finished_sandwiches = []
 
 while sandwich_orders:
-    (sandwich_orders)
+    sandwich_orders.reverse()
     order = sandwich_orders.pop()
-    print(f"i made your {order}")
+    print(f"i made your {order} sandwich")
     finished_sandwiches.append(order)
 
 print(f"\nready to eat sandwiches")
 for finished in finished_sandwiches:
-    print(f"\t{finished}")
+    print(f"\t{finished} sandwich")
+    
+    
+#! Task 2 : No Pastrami 
+# Using the list sandwich_orders, make sure the sandwich 'pastrami' appears in the list at least three times. 
+# Add code near the beginning of your program to print a message saying the deli has run out of pastrami, 
+# and then use a while loop to remove all occurrences of 'pastrami' from sandwich_orders. 
+# Make sure no pastrami sandwiches end up in finished_sandwiches
+print("====================================================================")
+
+sandwich_orders = ['pastrami','chicken', 'pastrami','egg','pastrami', 'beef']
+print("\noh No! our restaurant has run out of pastrami")
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+    
+while sandwich_orders:
+    sandwich_orders.reverse()
+    order = sandwich_orders.pop()
+    print(f"i made your {order} sandwich")
+    finished_sandwiches.append(order)
+
+print(f"\nready to eat sandwiches")
+for finished in finished_sandwiches:
+    print(f"\t{finished} sandwich")
