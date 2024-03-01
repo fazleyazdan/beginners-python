456c905b8f607ed6fdb91f68ce8357d50f47d0aa
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-01 22:53:32 +0500

    functions assignment 1 completed

---
+++ 09. functions/2. assignment-1.py
@@ -0,0 +1,19 @@
+# Task 1 Message: Write a function called display_message(),
+# that prints one sentence telling everyone what you are learning about in this chapter. 
+# Call the function, and make sure the message displays correctly
+
+def display_message():
+    print("i am learning functions in this chapter")
+display_message()
+
+# ===================================================== #
+
+#* Task 2 : Favorite Book: Write a function called favorite_book() that accepts one 
+#* parameter, title. The function should print a message, such as One of my 
+#* favorite books is a Metro. Call the function, making sure to 
+#* include a book title as an argument in the function call.
+
+def favorite_book(title):
+    print(f"\none of my favorite book is {title}.")
+    
+favorite_book('world of devil & jins')
