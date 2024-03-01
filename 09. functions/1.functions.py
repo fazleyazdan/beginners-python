af02384e511b2e9998374576705ac5ae05d779a1
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-01 22:24:56 +0500

    a simple function

---
+++ 09. functions/1.functions.py
@@ -0,0 +1,10 @@
+# Functions are named blocks of code designed to do one specific job.
+# If you need to perform that task multiple times throughout your program, 
+# you don’t need to type all the code for the same task again and again; 
+# you just call the function dedicated to handling that task, and the call 
+# tells Python to run the code inside the function.
+
+#* Simple function
+def greet_user():
+    print("Hello!\n")
+greet_user()
