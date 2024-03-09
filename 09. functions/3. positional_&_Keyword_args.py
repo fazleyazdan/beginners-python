8971ef501f452bdad40c12d74c86fe3f17435e58
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-09 23:00:02 +0500

    default values in function parameters

--- 09. functions/3. positional_&_Keyword_args.py
+++ 09. functions/3. positional_&_Keyword_args.py
@@ -16,10 +16,6 @@ describe_pet('snatcher', 'Parrot')
 
 #* ============================================================================ #
 
-#* A keyword argument is a name-value pair that you pass to a function. 
-# You directly associate the name and the value within the argument, so when you 
-# pass the argument to the function, there’s no confusion
-
 print("\n*** Keyword Arguments: ***")
 #* A keyword argument is a name-value pair that you pass to a function. 
 # You directly associate the name and the value within the argument, 
@@ -27,6 +23,21 @@ print("\n*** Keyword Arguments: ***")
 #!  When you use keyword arguments, be sure to use the exact names of the parameters in the function’s definition
 
 def describe_pet(pet_type = 'kangaroo', pet_name = 'jumper'):
-    print(f"\ni have {pet_type} and his name is {pet_name}")
+    print(f"\ni have {pet_type} and its name is {pet_name}")
 
 describe_pet()
+
+#* ============================================================================ #
+
+#* When writing a function, you can define a default value for each parameter. 
+# If an argument for a parameter is provided in the function call, Python uses the argument value.
+# If not, it uses the parameter’s default value. So when you define a default value for a parameter, 
+# you can exclude the corresponding argument you’d usually write in the function call.
+
+def default_val(pet_name, pet_type = 'Eagle'):
+    print(f"\ni have {pet_type} and its name is {pet_name}")
+
+default_val('hunter')
+
+#* you can also override the default value in function call
+default_val('hunter', pet_type='African Eagle')
