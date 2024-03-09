cd695a1e6963c9847fce49265cdd6092f671595c
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-09 21:58:24 +0500

    keyword arguments

--- 09. functions/3. positional_&_Keyword_args.py
+++ 09. functions/3. positional_&_Keyword_args.py
@@ -1,7 +1,7 @@
 # Order Matters in Positional Arguments
 # you have to pass arguments which matches the order of parameters.
 
-print("\n*** Positional Arguments: ***\n")
+print("\n*** Positional Arguments: ***")
 def describe_pet(pet_type, pet_name):
     print(f"\ni have {pet_type}")
     print(f"\tmy {pet_type} name is {pet_name.title()}")
@@ -20,4 +20,13 @@ describe_pet('snatcher', 'Parrot')
 # You directly associate the name and the value within the argument, so when you 
 # pass the argument to the function, there’s no confusion
 
-print("\n*** Keyword Arguments: ***\n")
+print("\n*** Keyword Arguments: ***")
+#* A keyword argument is a name-value pair that you pass to a function. 
+# You directly associate the name and the value within the argument, 
+# so when you pass the argument to the function, there’s no confusion.
+#!  When you use keyword arguments, be sure to use the exact names of the parameters in the function’s definition
+
+def describe_pet(pet_type = 'kangaroo', pet_name = 'jumper'):
+    print(f"\ni have {pet_type} and his name is {pet_name}")
+
+describe_pet()
