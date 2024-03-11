50c1d09f3628f804e8415905d41f87a7ae8176e7
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-11 22:48:11 +0500

    assignment 2 task 2 completed

--- 09. functions/4. assignment_2.py
+++ 09. functions/4. assignment_2.py
@@ -16,3 +16,21 @@ make_shirt('medium', "'be kind'")
 make_shirt(size='large', message="'be original in this fake world'")
 
 
+#! ==================== Task 2 ==================== #
+#* Large Shirts: Modify the make_shirt() function so that shirts are large 
+# by default with a message that reads I love Python. 
+# Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+
+#* default size
+def make_shirt_1(message, size = 'large'):
+    print(f"\nprint '{message}' on a {size} size T-shirt")
+make_shirt_1('I love python')
+
+#* default message
+def make_shirt_2(size, message = 'i love programming'):
+    print(f"\nprint '{message}' on a {size} size T-shirt")
+make_shirt_2(size = 'large')
+make_shirt_2(size='medium')
+
+#*different message
+make_shirt_2(size='small', message='peace be upon you')
