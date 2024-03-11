83b96c5717ee1fb49517c2c1c227a39a7e159fe3
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-11 22:52:31 +0500

    assignment 2 task 3 completed

--- 09. functions/4. assignment_2.py
+++ 09. functions/4. assignment_2.py
@@ -34,3 +34,16 @@ make_shirt_2(size='medium')
 
 #*different message
 make_shirt_2(size='small', message='peace be upon you')
+
+#! ==================== Task 3 ==================== #
+#* Cities: Write a function called describe_city() that accepts the name of a city and its country. 
+# The function should print a simple sentence, such as Reykjavik is in Iceland. 
+# Give the parameter for the country a default value. 
+# Call your function for three different cities, at least one of which is not in the default country.
+
+def describe_city(city_name, country = 'Pakistan'):
+    print(f"\n{city_name.title()} is in {country}")
+    
+describe_city('islamabad')
+describe_city('peshawar')
+describe_city('madina', country='saudi arabia')    
