2335c6cedcee5f9e93ab24a9913dc2e03f50c678
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-12 23:19:44 +0500

    git formatted name

--- 09. functions/5. return_values.py
+++ 09. functions/5. return_values.py
@@ -2,12 +2,34 @@
 # Instead, it can process some data and then return a value or set of values. 
 #* The value the function returns is called a return value.
 
-def return_fname(first, last):
+def get_formatted_name(first, last):
     """Return Neatly formated full name"""
     full_name = f"{first} {last}"
     return full_name.title()
 
-my_name= return_fname('fazle', 'yazdan')
 #* When you call a function that returns a value, you need to provide a variable that the return value can be assigned to.
-print(my_name)
+my_name= get_formatted_name('fazle', 'yazdan')
+print(f"\n{my_name}")
 
+#! ================================================ # 
+
+#! Making an Argument Optional
+#* we want to expand get_formatted_name() to handle middle names as well. 
+# But middle names aren’t always needed, 
+# To make get_formatted_name() work without a middle name, 
+# we set the default value of middle_name to an empty string and move it to the end of the list of parameters
+
+def get_middle_name(first, last, middle = ''):
+    """Return Neatly formated full name"""               
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    else:
+        full_name = f"{first} {last}"
+        
+    return full_name.title()
+
+#* When you call a function that returns a value, you need to provide a variable that the return value can be assigned to.
+my_name= get_middle_name('fazle', 'yazdan')
+print(f"\n{my_name}")
+my_name= get_middle_name('khabib', 'nur', 'the eagle')
+print(f"\n{my_name}")
