258013b4e23ce56c83bfc12753ddfe3f51513f89
fazleyazdan <fazleyazdan345@gmail.com>
2024-04-07 12:51:36 +0500

    Task 3 completed

--- 09. functions/3.4. assignment.py
+++ 09. functions/3.4. assignment.py
@@ -3,7 +3,7 @@
 # and it should print a summary of the sandwich that’s being ordered. 
 # Call the function three times, using a different number of arguments each time.
 
-print("\n------------ Task 1 ------------")
+print("\n------------ Task 1 ------------\n")
 
 def make_sandwich(*items):
     print(f"\nMaking sandwich with the Following items:")
@@ -19,7 +19,7 @@ make_sandwich('egg', 'salad', 'beef')
 # Build a profile of yourself by calling build_profile(), 
 # using your first and last names and three other key-value pairs that describe you 
 
-print("\n------------ Task 2 ------------")
+print("\n------------ Task 2 ------------\n")
 
 def build_profile(first, last, **user_info):
     user_info['first_name'] = first
@@ -27,4 +27,25 @@ def build_profile(first, last, **user_info):
     return user_info
 
 userInfo = build_profile('fazle', 'yazdan', location = 'Islamabad', field = 'SQA', favorite_food = 'dates')
-print(f"\n{userInfo}\n")
+print(f"{userInfo}\n")
+
+
+print("\n------------ Task 3 ------------\n")
+
+# Cars: Write a function that stores information about a car in a dictionary. 
+# The function should always receive a manufacturer and a model name. 
+# It should then accept an arbitrary number of keyword arguments. 
+# Call the function with the required information and two other name-value pairs, 
+# such as a color or an optional feature. Your function should work for a call like this one:
+
+# car = make_car('subaru', 'outback', color='blue', tow_package=True)
+# Print the dictionary that’s returned to make sure all the information was stored correctly
+
+def car_info(manufacturer, model_name, **info):
+    print("info about the car:")
+    info['manufacturer'] = manufacturer
+    info['model'] = model_name
+    return info
+    
+store_info = car_info('Tesla', '2023', color = 'white', tow_package = 'True')
+print(store_info)
