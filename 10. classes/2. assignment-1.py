b4992b5cd546ca8dc627bb747b6026d92eb6da33
fazleyazdan <fazleyazdan345@gmail.com>
2024-05-25 23:40:13 +0500

    assignment 1 task 3 completed

--- 10. classes/2. assignment-1.py
+++ 10. classes/2. assignment-1.py
@@ -40,3 +40,39 @@ restaurant_3 = Restaurant('Khyber Shinwari', 'beef karahi')
 restaurant_1.describe_restaurant()
 restaurant_2.describe_restaurant()
 restaurant_3.describe_restaurant()
+
+
+print("\n------------ Task 3 ------------\n")
+
+# Users: Make a class called User. Create two attributes called first_name
+# and last_name, and then create several other attributes that are typically stored in a user profile. 
+# Make a method called describe_user() that prints a summary of the user’s information. 
+# Make another method called greet_user() that prints a personalized greeting to the user.
+# Create several instances representing different users, and call both methods for each user.
+
+class User():
+    
+    def __init__(self,first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        
+    def describe_user(self, age, profession):
+        print(f"\tUser name: {self.first_name + ' ' + self.last_name}")
+        print(f"\tUser age: {age}")
+        print(f"\tUser profession: {profession}")
+        
+    def greet_user(self):
+        print(f"\nHello {self.first_name + ' ' + self.last_name}! Welcome to beginners Python")
+        
+        
+person = User('ali', 'khan')
+person.describe_user(20, 'Rescuer')
+
+person_1 = User('fazle', 'yazdan')
+person_1.greet_user()
+person_1.describe_user(25, 'Software Tester')
+
+person_2 = User('jawad', 'khan')
+person_2.greet_user()
+person_2.describe_user(24, 'Quality Assurance')
+
