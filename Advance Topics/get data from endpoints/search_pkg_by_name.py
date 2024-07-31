d2cca80fa76cce36053fe10a17873dfb5f01ca31
fazleyazdan <fazleyazdan345@gmail.com>
2024-07-31 17:43:34 +0500

    Search Package by name

--- Advance Topics/get data from endpoints/search_pkg_by_name.py
+++ Advance Topics/get data from endpoints/search_pkg_by_name.py
@@ -4,41 +4,27 @@ from munch import Munch
 # Define the necessary variables
 PACKAGE_NAME = 'multikiwilogger'
 
-def search_pkg_by_name(package_name):
+def get_package_details_by_name(package_name):
     end_point = f"https://api.complyvantage.com/api/packages/find-by-package-name/{package_name}"
     response = requests.get(end_point)
-    list_items = response['items']
+    return response.json()
 
 def test_search_pkg_by_name():
-    
-    end_point = f"https://api.complyvantage.com/api/packages/find-by-package-name/{PACKAGE_NAME}"
+    response = get_package_details_by_name(PACKAGE_NAME)
+    list_items = response.get('items', [])
 
-    # Make the get request to get package details
-    response = requests.get(end_point)
-    return response.json()
-    
-response = test_search_pkg_by_name()
-list_items = response['items']
+    # Check if the response contains the package name
+    package_names = [item['name'] for item in list_items if 'name' in item]
+    assert PACKAGE_NAME in package_names, f"Package name '{PACKAGE_NAME}' not found in response items"
+
+if __name__ == "__main__":
+    test_search_pkg_by_name()
+    print("Test Case Passed!")
 
 
-for list in list_items:
-    for k,v in list.items():
-        if v == 'multikiwilogger':
-            print("Test Case Passed !")
-            break
-
-        
-# print(split_response)
-
-# if split_response.name == "multikiwilogger":
-#     print("Test Case Passed !")
-# else:
-#     print("Test Case Failed")
-    
-# list_of_dict = split_response['items']
-
-# for list in list_of_dict:
-#     print(list)
-    # for k,v in list.items():
-    #     print(f"{k} : {v}")
-
+test_search_pkg_by_name()
+# for list in list_items:
+#     for k,v in list.items():
+#         if v == 'multikiwilogger':
+#             print("Test Case Passed !")
+#             break
