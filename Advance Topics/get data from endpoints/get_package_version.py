55e41ce439490de769cfd8efe3486ab78b7d2557
fazleyazdan <fazleyazdan345@gmail.com>
2024-08-02 11:03:32 +0500

    get package by version

--- Advance Topics/get data from endpoints/get_package_version.py
+++ Advance Topics/get data from endpoints/get_package_version.py
@@ -8,3 +8,17 @@ def test_get_pkg_version():
     end_point = f'https://api.complyvantage.com/api/packages/{PACKAGE_ID}/versions'
     response = requests.get(end_point)
     
+    assert response.status_code == 200
+    response = response.json()
+    list_version = response['items']
+    
+    if len(list_version) > 0:
+        print(len(list_version))
+        assert True
+    else:
+        print("test case failed!")
+        assert False
+    
+test_get_pkg_version()
+    
+    
