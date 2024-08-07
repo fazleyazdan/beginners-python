853d8f0e72681ddcb44aca6dccee79cd9539f9cb
fazleyazdan <fazleyazdan345@gmail.com>
2024-08-07 12:25:52 +0500

    check palindrome

---
+++ 12. interview/equal.py
@@ -0,0 +1,8 @@
+email_token = '20971647-15b5-4301-9c26-d2268dc62e5b'
+admin_token = '20971647-15b5-4301-9c26-d2268dc62e5b'
+
+if email_token == admin_token:
+    print ("true")
+else:
+    print ("false")
+    
--- 12. interview/palindrome.py
+++ 12. interview/palindrome.py
@@ -11,3 +11,22 @@ def check_if_palindrome(word):
 
 word = 'racecar'
 print(f"{word} is palindrome:", check_if_palindrome(word))
+
+
+
+
+# How the -1 Works in Indexing:
+#* Length of the String:
+
+#* n = len(s) gets the length of the string s.
+# Loop through the String:
+
+#* for i in range(n // 2) loops from 0 to n // 2 - 1, which is the midpoint of the string. This ensures we only compare each pair of characters once.
+# Character Comparison:
+
+#* s[i] accesses the character at index i from the start of the string.
+# s[n - i - 1] accesses the character at index n - i - 1 from the end of the string.
+# For i = 0, s[i] is the first character and s[n - i - 1] is the last character.
+# For i = 1, s[i] is the second character and s[n - i - 1] is the second-to-last character.
+# This continues until the midpoint of the string.
+
