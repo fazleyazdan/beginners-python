758a9331d8d2bfbc4d54ca5159405c0aaa14e36b
fazleyazdan <fazleyazdan345@gmail.com>
2024-08-22 21:32:20 +0500

    get total num packages from DB

---
+++ Advance Topics/get data from endpoints/get_number_packages.py
@@ -0,0 +1,32 @@
+import boto3
+from botocore.exceptions import ClientError
+
+dynamodb = boto3.client('dynamodb', region_name='eu-central-1')
+
+def count_packages_by_language_paginator(table_name, language):
+    paginator = dynamodb.get_paginator('scan')
+    count = 0
+
+    try:
+        for page in paginator.paginate(
+            TableName=table_name,
+            FilterExpression="#lang = :lang",
+            ExpressionAttributeNames={"#lang": "language"},
+            ExpressionAttributeValues={":lang": {"S": language}},
+            Select="COUNT"
+        ):
+            count += page.get('Count', 0)
+    except ClientError as e:
+        raise Exception(f"Error scanning table with paginator: {e}")
+
+    return count
+
+# Example usage
+if __name__ == "__main__":
+    table_name = 'package-prd'
+    language = 'javascript'  # Replace with the language you want to query
+    count = count_packages_by_language_paginator(table_name, language)
+    print(f"Total number of packages with language '{language}': {count}")
+
+
+# ! working but too much time taking
