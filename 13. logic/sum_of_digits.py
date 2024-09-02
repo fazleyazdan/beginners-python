57deb5913eb50244f9429346b8bd2bf864f014c6
fazleyazdan <fazleyazdan345@gmail.com>
2024-09-02 15:05:02 +0500

    logic building

--- 13. logic/sum_of_digits.py
+++ 13. logic/sum_of_digits.py
@@ -8,7 +8,7 @@ def sum_of_digits(num):
     str_num = str(num)         # Convert the integer to a string to iterate over each digit
     for digit in str_num:      # Iterate over each character in the string
         sum += int(digit)      # Convert the character back to an integer and add it to the sum
-    return sum                 # return total_sum
+    return sum                 # return total_sum 
 
 print(sum_of_digits(43))       # Output 7
 
