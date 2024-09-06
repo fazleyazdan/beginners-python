b77df8373bb96c21a7caaf861d5fe6e569798e2a
fazleyazdan <fazleyazdan345@gmail.com>
2024-09-06 14:41:09 +0500

    count digits

---
+++ 13. logic/beginners/count_digits.py
@@ -0,0 +1,15 @@
+# Count the Number of Digits
+# Problem: Write a Python function that takes an integer as input and counts the number of digits in it.
+# Input: An integer n.
+# Output: The number of digits in the integer
+
+
+def count_digits(integer):
+    str_integer = str(integer)
+    count = len(str_integer)
+    return count
+
+print(count_digits(231235))        # output : 6
+
+# alternative you can convert the int to string then apply loop on it, and increment the counter for each iteration.
+
