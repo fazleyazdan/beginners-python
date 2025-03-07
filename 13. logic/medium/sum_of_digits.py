39c74e66146d0b7db5501ef980e7d6d59ba46d0c
fazleyazdan <fazleyazdan345@gmail.com>
2025-03-07 23:27:47 +0500

    sum of digits

--- 13. logic/medium/sum_of_digits.py
+++ 13. logic/medium/sum_of_digits.py
@@ -3,6 +3,13 @@
 # Output: An integer representing the sum of the digits of n
 
 
+def sum_of_digits(num):
+    sum = 0                    # Initialize sum to 0
+    str_num = str(num)         # Convert the integer to a string to iterate over each digit
+    for digit in str_num:      # Iterate over each character in the string
+        sum += int(digit)      # Convert the character back to an integer and add it to the sum
+    return sum                 # return total_sum 
 
+print(sum_of_digits(43))       # Output 7
 
     
