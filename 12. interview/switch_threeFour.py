a7c7c3ba42a9465cd66d22572c8fdebe339c56cf
fazleyazdan <fazleyazdan345@gmail.com>
2025-06-18 17:58:23 +0500

    task 1 switching

--- 12. interview/switch_threeFour.py
+++ 12. interview/switch_threeFour.py
@@ -1,3 +1,15 @@
+# write a function , which takes 3 or 4. when it takes 3 it return 4. and when it takes 4 it return 3. 
+#! Note : don't use conditions or any built in method
+
+def rotate_three_four(num):
+    dict = {3:4 , 4:3}
+    return dict[num]
+
+num = rotate_three_four(4)
+num1 = rotate_three_four(3)
+print(num)
+print(num1)
+
 
 #! ======= OR =======
 
