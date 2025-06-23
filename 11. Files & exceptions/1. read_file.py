f0e706eb9d7467d47f34bf117cdf6a74794b066b
fazleyazdan <fazleyazdan345@gmail.com>
2025-06-23 18:30:50 +0500

    file reading

--- 11. Files & exceptions/1. read_file.py
+++ 11. Files & exceptions/1. read_file.py
@@ -8,6 +8,15 @@ they encounter bad data, whether it comes from innocent mistakes or from malicio
 #* When you want to work with the information in a text file, the first step is to read the file into memory. 
 #* You can then work through all of the file’s contents at once or work through the contents line by line.
 
+""" 
+To begin, we need a file with a few lines of text in it. Let's start with a file 
+that contains pi to 30 decimal places, with 10 decimal places per line:
+ 
+3.1415926535
+  8979323846
+  2643383279
+
+"""
 
 from pathlib import Path
 
