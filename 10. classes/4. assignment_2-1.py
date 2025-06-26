0eeb2ef478ca134e75dc4c2a8218f423949bc92c
fazleyazdan <fazleyazdan345@gmail.com>
2025-06-26 23:02:26 +0500

    assignment 2 task 1

--- 10. classes/4. assignment_2-1.py
+++ 10. classes/4. assignment_2-1.py
@@ -10,3 +10,46 @@
 # the number of customers who’ve been served. Call this method with any number 
 # you like that could represent how many customers were served in, say, a day of business
 
+class Restaurant():
+    
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+        
+              
+    def describe_restaurant(self):
+        print(f"Restaurant Name: {self.restaurant_name}")
+        print(f"cuisine type: {self.cuisine_type}")
+        
+        
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is open now")
+        
+        
+    def set_number_served(self, noCustomers):
+        self.number_served = noCustomers
+
+
+    def increment_number_served(self, incrementVal):
+        if incrementVal < 0:
+            print(f"\nnumber of customer should not be in Negative")
+        else:
+            self.number_served += incrementVal
+        
+            
+restaurant = Restaurant('Dam Pookh', 'Steak')
+
+print(f"\n{restaurant.restaurant_name} has served {restaurant.number_served} customers") 
+restaurant.number_served = 7
+print(f"\n{restaurant.restaurant_name} has served {restaurant.number_served} customers") 
+       
+#! num of customer served with a method
+
+restaurant.set_number_served(8)
+print(f"\nTotal number of served customers by {restaurant.restaurant_name} : {restaurant.number_served}")
+
+#! Incrementing number of served Customers
+
+restaurant.increment_number_served(-5)
+print(f"\nTotal number of served customers after Increment: {restaurant.number_served}")
