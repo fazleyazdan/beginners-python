57116ffe1071c46b0833e5775ef9d16a28e95723
fazleyazdan <fazleyazdan345@gmail.com>
2025-07-08 18:36:08 +0500

    py standard lib

---
+++ 10. classes/8. py_standard_lib.py
@@ -0,0 +1,8 @@
+# The Python Standard Library
+""" The Python standard library is a set of modules included with every Python 
+installation. Now that you have a basic understanding of how functions and 
+classes work, you can start to use modules like these that other programmers 
+have written. You can use any function or class in the standard library by 
+including a simple import statement at the top of your file. Let's look at one 
+module, random, which can be useful in modeling many real-world situations. """
+
