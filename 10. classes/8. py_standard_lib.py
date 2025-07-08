ac8b221a4b80833594f3ca253d1879bc6ce0a409
fazleyazdan <fazleyazdan345@gmail.com>
2025-07-08 18:36:27 +0500

    py standard library

--- 10. classes/8. py_standard_lib.py
+++ 10. classes/8. py_standard_lib.py
@@ -10,3 +10,17 @@ module, random, which can be useful in modeling many real-world situations. """
 # This function takes two integer arguments and returns a randomly selected integer between (and including) those numbers.
 # Here’s how to generate a random number between 1 and 6:
 
+from random import randint
+from random import choice
+
+integer = randint(1,7)
+print(integer)              #! every time you run it , random number will be selected
+
+#* Another useful function is choice(). This function takes in a list or tuple and returns a randomly chosen element:
+
+players = ['fazle yazdan', 'jawad', 'arsalan', 'zakwan', 'ali']
+print(choice(players))
+
+
+#! random module shouldn’t be used when building security-related applications, 
+#! but it works well for many fun and interesting projects.
