e8ee12784302e55b5298025bbd729abc81d6605e
fazleyazdan <fazleyazdan345@gmail.com>
2025-09-20 22:29:41 +0500

    user inputs

--- 08. userinputs & while loops/4. userinput_&_dictionary.py
+++ 08. userinputs & while loops/4. userinput_&_dictionary.py
@@ -1,10 +1,3 @@
-# Filling a Dictionary with User Input
-#* we can take user input as much as we want
-#* lets do a poll, asking users about their favorite programming languages.
-
-languages = {}
-# Set a flag to indicate that polling is active.
-flag = True
 
 while flag:
     name = input("\nplease enter your name: ")
