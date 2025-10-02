2fd9c1a791b9c061c63691aa96f2d0c4f2c3e48c
fazleyazdan <fazleyazdan345@gmail.com>
2025-10-02 18:55:59 +0500

    task 4

---
+++ 04.1. advance_Lists/task_4.py
@@ -0,0 +1,7 @@
+# Counting to Twenty: Use a for loop to print the numbers from 1 to 20, inclusive.
+
+for value in range(1,21):
+    print(value)
+    
+#  Make a list of the numbers from one to one million, and then 
+# use a for loop to print the numbers. (If the output is taking too long, stop it by 
