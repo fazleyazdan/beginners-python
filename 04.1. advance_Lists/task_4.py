776df987b6c73b755de69e8904ebcd4b136b7c10
fazleyazdan <fazleyazdan345@gmail.com>
2025-10-02 18:56:36 +0500

    task 4 lists

--- 04.1. advance_Lists/task_4.py
+++ 04.1. advance_Lists/task_4.py
@@ -33,3 +33,18 @@ multiple_3 = [value*3 for value in range(1,11)]            # list comprehension
 for multiple in multiple_3:
     print(multiple)
     
+# A number raised to the third power is called a cube. For example, 
+# the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes 
+# (that is, the cube of each integer from 1 through 10), and use a for loop to print out the value of each cube
+
+cubes = []
+
+for value in range(1,11):
+    cube = value**3
+    cubes.append(cube)
+    print(cube)
+    
+# Use a list comprehension to generate a list of the first 10 cubes.
+
+cubes_comprehension = [value**3 for value in range(1,11)]
+print(cubes_comprehension)
