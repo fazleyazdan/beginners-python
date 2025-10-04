70295aa9e0935ab67a76b764f755a2a0a8632819
fazleyazdan <fazleyazdan345@gmail.com>
2025-10-04 14:44:58 +0500

    assignment extras

--- 06. Conditions & operators/assignment_admin.py
+++ 06. Conditions & operators/assignment_admin.py
@@ -13,4 +13,18 @@ for user in usernames:
         print(f"welcome {user}!")
 
         
+# No Users: Add an if test to hello_admin.py to make sure the list of users is not empty.
+# • If the list is empty, print the message We need to find some users!
+# • Remove all of the usernames from your list, and make sure the correct message is printed.
+
+while usernames:
+    usernames.pop()
+    
+if usernames:
+    print("hello all")
+else:
+    print("sorry there are no users left")
+
+print(usernames)
+
 
