3a0d33e773d5760ee166630635648e5f87016379
fazleyazdan <fazleyazdan345@gmail.com>
2025-10-04 14:44:37 +0500

    list of users

--- 06. Conditions & operators/assignment_admin.py
+++ 06. Conditions & operators/assignment_admin.py
@@ -1,1 +1,16 @@
+# Make a list of five or more usernames, including the name 'admin'. 
+# Imagine you are writing code that will print a greeting to each user after they log in to a website. 
+# Loop through the list, and print a greeting to each user.
+# • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+# • Otherwise, print a generic greeting, such as Hello Jaden, thank you for logging in again
+
+usernames = ['ali', 'kamran', 'shadman', 'admin', 'abdullah']
+
+for user in usernames:
+    if user == 'admin':
+        print(f"Hello {user}, would you like to see a status report?")
+    else:
+        print(f"welcome {user}!")
+
+        
 
