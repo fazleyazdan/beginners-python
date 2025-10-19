4be64d1bbfcdca8a9d64e35409504b63abfa4972
fazleyazdan <fazleyazdan345@gmail.com>
2025-10-19 20:07:50 +0500

    comparison operators

--- 06. Conditions & operators/comparison_operators.py
+++ 06. Conditions & operators/comparison_operators.py
@@ -1,3 +1,18 @@
 # 1. use comparison operators
 # 2. use if elif condition
  
+age = 12
+
+if age <= 4:
+    price = 0
+
+if age <= 10:
+    price = 10
+
+if age >= 12:
+    price = 10
+
+print("your admission fee is " + str(price) + "$.")
+
+
+
