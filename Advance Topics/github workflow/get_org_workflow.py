406661ab7962540b96063cb34039c4e34cf5051b
fazleyazdan <fazleyazdan345@gmail.com>
2025-11-16 22:42:26 +0500

    get org workflow

--- Advance Topics/github workflow/get_org_workflow.py
+++ Advance Topics/github workflow/get_org_workflow.py
@@ -9,3 +9,12 @@ WORKFLOW_ID = 'trigger_kb_update.yml'  # or you can use the workflow ID
 # Set up the API endpoint to get the workflow information
 url = f"https://api.github.com/repos/{OWNER}/{REPO}/actions/workflows/{WORKFLOW_ID}"
 
+# Set up the headers with authentication
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {GITHUB_TOKEN}"
+}
+
+# Make the GET request to retrieve the workflow information
+response = requests.get(url, headers=headers)
+
