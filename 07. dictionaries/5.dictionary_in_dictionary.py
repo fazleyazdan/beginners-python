611fd06118c9e25f271a620cc9846380bcfa34f1
fazleyazdan <fazleyazdan345@gmail.com>
2025-11-26 22:28:22 +0500

    task 1

--- 07. dictionaries/5.dictionary_in_dictionary.py
+++ 07. dictionaries/5.dictionary_in_dictionary.py
@@ -1,2 +1,9 @@
+# You can nest a dictionary inside another dictionary, 
+#* but your code can get complicated quickly when you do. 
+# For example, if you have several users for a website, each with a unique username, 
+#* you can use the usernames as the keys in a dictionary. 
+# You can then store information about each user by using a dictionary 
+#* as the value associated with their username.
+
 
     
