6f0c8d2cc29e0563f78d1e0f83e460ca4bf70def
fazleyazdan <fazleyazdan345@gmail.com>
2025-11-29 20:00:32 +0500

    task 3

--- 10. classes/assignment 4/task_three.py
+++ 10. classes/assignment 4/task_three.py
@@ -1,1 +1,5 @@
+# Lottery Analysis: You can use a loop to see how hard it might be to win the kind of lottery you just modeled. 
+# Make a list or tuple called my_ticket. 
+# Write a loop that keeps pulling numbers until your ticket wins. 
+# Print a message reporting how many times the loop had to run to give you a winning ticket.
 
