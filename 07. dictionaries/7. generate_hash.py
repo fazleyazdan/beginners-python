2ce53be76534cd71eded24ce5a5bfeb9ba0584a1
fazleyazdan <fazleyazdan345@gmail.com>
2025-12-06 22:34:41 +0500

    hash

---
+++ 07. dictionaries/7. generate_hash.py
@@ -0,0 +1,18 @@
+# here in this task we will generate different types of hashes for a file
+
+''' In simple terms, a hash is like a fingerprint for data. Just as each person has a unique fingerprint, 
+each piece of data (like a file or a piece of text) has a unique hash. 
+
+Imagine you have a magic machine that takes any object you give it—let's say a book—and it squishes it down into a small, 
+fixed-size piece of metal. No matter how big the book is, the piece of metal is always the same size. 
+This piece of metal is the hash.
+
+Now, if you change even a tiny bit of the book—like adding or removing a single word—the hash will look completely different.
+It's as if changing one word in the book would completely change the shape of the piece of metal.
+
+So, hashes are used to verify if data has changed. For example, if you download a file from the internet, you can calculate its hash and compare it to the hash provided by the original source. If they match, it means the file is exactly the same as when it was originally created. 
+If they don't match, it means the file has been altered in some way.
+
+Hash cannot be reversed to retrieve the original data.
+'''
+
