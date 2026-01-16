44157a35d0942c5f84643d6b72f3430e8d46c25b
fazleyazdan <fazleyazdan345@gmail.com>
2026-01-16 23:29:38 +0500

    generate hashes

--- 07. dictionaries/7. generate_hash.py
+++ 07. dictionaries/7. generate_hash.py
@@ -7,3 +7,13 @@ Imagine you have a magic machine that takes any object you give it—let's say a
 fixed-size piece of metal. No matter how big the book is, the piece of metal is always the same size. 
 This piece of metal is the hash.
 
+Now, if you change even a tiny bit of the book—like adding or removing a single word—the hash will look completely different.
+It's as if changing one word in the book would completely change the shape of the piece of metal.
+
+So, hashes are used to verify if data has changed. For example, if you download a file from the internet, you can calculate its hash and compare it to the hash provided by the original source. If they match, it means the file is exactly the same as when it was originally created. 
+If they don't match, it means the file has been altered in some way.
+
+Hash cannot be reversed to retrieve the original data.
+'''
+
+import hashlib
