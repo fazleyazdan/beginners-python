5158412a6d760b864b0726b7de1137eb663a8c94
fazleyazdan <fazleyazdan345@gmail.com>
2026-01-16 23:29:33 +0500

    generate hashes

--- 07. dictionaries/7. generate_hash.py
+++ 07. dictionaries/7. generate_hash.py
@@ -1,1 +1,9 @@
+# here in this task we will generate different types of hashes for a file
+
+''' In simple terms, a hash is like a fingerprint for data. Just as each person has a unique fingerprint, 
+each piece of data (like a file or a piece of text) has a unique hash. 
+
+Imagine you have a magic machine that takes any object you give it—let's say a book—and it squishes it down into a small, 
+fixed-size piece of metal. No matter how big the book is, the piece of metal is always the same size. 
+This piece of metal is the hash.
 
