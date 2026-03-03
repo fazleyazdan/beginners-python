63e1931eb013ffd3756610519ce67a4c350cec4f
fazleyazdan <fazleyazdan345@gmail.com>
2026-03-03 23:58:27 +0500

    munch

---
+++ 07. dictionaries/6. using munch.py
@@ -0,0 +1,10 @@
+# munch is a Python library that provides a dictionary subclass which allows keys to be accessed as attributes. 
+# This means that, in addition to using the standard dictionary syntax to access values, you can use dot notation, 
+# which can make the code cleaner and more readable.
+#* to use munch first install the library using 'pip install munch' & then import it
+
+#! here is an example to demonstrate the usage of munch:
+
+from munch import Munch
+
+
