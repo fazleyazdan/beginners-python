87d835cf3590cba03856c7acab4cb0c3972a3c5d
fazleyazdan <fazleyazdan345@gmail.com>
2026-03-05 10:52:04 +0500

    function format

--- 09. functions/1.1. styling_function.py
+++ 09. functions/1.1. styling_function.py
@@ -1,2 +1,10 @@
 
+#! Styling Functions
+
+''' You need to keep a few details in mind when you're styling functions. 
+Functions should have descriptive names, and these names should use 
+lowercase letters and underscores. Descriptive names help you and others 
+understand what your code is trying to do. Module names should use these 
+conventions as well.
+
 
