1f642b8cc3d6ce350b46b363959260874a80424e
fazleyazdan <fazleyazdan345@gmail.com>
2026-03-05 10:52:13 +0500

    function format

--- 09. functions/1.1. styling_function.py
+++ 09. functions/1.1. styling_function.py
@@ -7,4 +7,21 @@ lowercase letters and underscores. Descriptive names help you and others
 understand what your code is trying to do. Module names should use these 
 conventions as well.
 
+Every function should have a comment that explains concisely what 
+the function does. This comment should appear immediately after the 
+function definition and use the docstring format. In a well-documented 
+function, other programmers can use the function by reading only the 
+description in the docstring. They should be able to trust that the code 
+works as described, and as long as they know the name of the function, the 
+arguments it needs, and the kind of value it returns, they should be able to 
+use it in their programs.
+
+If you specify a default value for a parameter, no spaces should be used 
+on either side of the equal sign: 
+e.g.
+def function_name(parameter_0, parameter_1='default value')
+
+The same convention should be used for keyword arguments in function calls:
+e.g.
+function_name(value_0, parameter_1='value') 
 
