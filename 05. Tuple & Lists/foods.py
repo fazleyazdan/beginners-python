bdd097f355b9a25f2f7b9faee3a7e9a0b190e536
fazleyazdan <fazleyazdan345@gmail.com>
2026-04-16 23:34:56 +0500

    list copying

--- 05. Tuple & Lists/foods.py
+++ 05. Tuple & Lists/foods.py
@@ -1,2 +1,9 @@
+# You are creating two lists, my_foods and friend_foods, and making a copy of the elements from my_foods into friend_foods.
+# The [:] syntax copies all elements from the beginning to the end of the list.
+
+my_foods = ['talbina' , 'honey' , 'red beans']
+
+friend_foods = my_foods[:]
+
 
 
