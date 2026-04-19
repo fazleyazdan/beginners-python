7b2036af9a0fdf268f241f0739ab6dab8484919f
fazleyazdan <fazleyazdan345@gmail.com>
2026-04-19 23:25:38 +0500

    2 of 1

---
+++ 10. classes/4. assignment_2-1.py
@@ -0,0 +1,32 @@
+# Number Served: Start with your program from Task 1 of assignment-1. 
+# Add an attribute called number_served with a default value of 0. 
+# Create an instance called restaurant from this class. 
+# Print the number of customers the restaurant has served, and then change this value and print it again.
+
+# Add a method called set_number_served() that lets you set the number of 
+# customers that have been served. Call this method with a new number and print the value again.
+
+# Add a method called increment_number_served() that lets you increment 
+# the number of customers who’ve been served. Call this method with any number 
+# you like that could represent how many customers were served in, say, a day of business
+
+class Restaurant():
+    
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+        
+              
+    def describe_restaurant(self):
+        print(f"Restaurant Name: {self.restaurant_name}")
+        print(f"cuisine type: {self.cuisine_type}")
+        
+        
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is open now")
+        
+        
+    def set_number_served(self, noCustomers):
+        self.number_served = noCustomers
+
