5727b6a9ec8d0b156311b90d064b991ba138639f
fazleyazdan <49106043+fazleyazdan@users.noreply.github.com>
2026-06-13 23:05:07 +0500

    Update greeting.py

--- 03. strings/greeting.py
+++ 03. strings/greeting.py
@@ -1,5 +1,5 @@
 # this code prints pairs of names from the friends list in the format "First_Name Last_Name". 
-#* Note that this code assumes that the number of names is even and that each name consists of a first name and a last name. 
+#* Note that this code assumes that the number of names is even and that each name consists of a first name and a last name.  
 # If the number of names is odd or if names are formatted differently, the code may not behave as expected.
 # friends = """ ... """.split(): This creates a multiline string with names separated by newlines 
 # and then uses the split() method to convert it into a list of names. Each name becomes an element in the list. 
